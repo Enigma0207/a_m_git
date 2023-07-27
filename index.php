6d6ebd91af48c1271341bf6d9238b2ed5400b851
alin <alinmansita@gmail.com>
2023-07-27 10:52:30 +0200

    alin_projet

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <div class="form1">
+        <form>
+            <label class="first" for="text">firstname</label>
+            <input type="text" id="firstname" name="nom1" required placeholder="firstname"> <br><br>
+            <label class="last" for="text">lastnam</label>
+            <input type="text" id="lastname" name="nom2" required placeholder="lastname"> <br><br>
+            <label class="email" for="text">emal</label>
+            
+            <input type="text" id="email" name="nom3" required placeholder="email"><br><br>
+            <label class="mdp" for="motdepasse">Mot de passe</label>
+            <input type="password" id="motdepasse" name="motdepasse" required placeholder="mdp">
+            <br>
+            <button type="submit">envoyer</button>
+        </form>
+    </div>
+</body>
+</html>
+
